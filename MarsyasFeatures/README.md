93a8021b4e2a1354635843dd9d0540afd2c02ff7
ctralie <chris.tralie@gmail.com>
2014-05-26 20:13:40 -0400

    Updated readmes and added capability to save mat file along with ARFF file when extracting feature averages from Marsyas

--- MarsyasFeatures/README.md
+++ MarsyasFeatures/README.md
@@ -9,14 +9,12 @@ Marsyas Installation Directions/Tricks
 First download and install the Marsyas suite
 http://sourceforge.net/projects/marsyas/files/
 with the usual sequence of commands
-*cmake .
-*make all
-*sudo make install
+* cmake .
+* make all
+* sudo make install
 
 Pay attention to where the shared library libmarsyas.so gets copied.  On my OS it got copied to /usr/local/lib, and it couldn't be found there, so I had to make a symbolic link to /usr/lib
 
 Using scripts
 --------------
-*Run the script "makeARFFFileAveraged.py" to make an ARFF file of the features averaged over the entire song
-
-
+* Run the script "makeARFFFileAveraged.py" to make an ARFF file of the features averaged over each entire song.  I relied on the ARFF file more towards the beginning of my development when I was checking the features in Weka to make sure they were meaningful.  I updated the program to also save a .mat file, which is more useful for the rest of the TDA stuff
--- MarsyasFeatures/makeARFFFileAveraged.py
+++ MarsyasFeatures/makeARFFFileAveraged.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+import numpy as np
+import scipy.io as sio
 
 if __name__ == '__main__':
 	genreIndex = open('../MusicDownloader/Music/index.txt', 'r')
@@ -23,16 +25,18 @@ if __name__ == '__main__':
 	arffFile.write("@attribute album string\n")
 	arffFile.write("@attribute title string\n")
 	arffFile.write("\n\n@data\n")
-	
+	attributes = [a.split("@attribute ")[1] for a in attributes]
 	
 	os.chdir('../MusicDownloader/Music')
 	dirNum = 0
+	songsFeatures = np.array([])
+	songsInfo = []
 	for genre in genreNames:
 		songsIndex = open("%i/index.txt"%dirNum, 'r')
 		songsLines = songsIndex.readlines()
 		songsLines = [s.rstrip() for s in songsLines]
 		#This now assumes additional data has been collected from discogs
-		for i in range(0, len(songsLines)/6): 
+		for i in range(0, min(len(songsLines)/6, 2)): 
 			#filename, song.artist, song.album, song.title
 			i1 = i*6
 			filename = songsLines[i1]
@@ -41,6 +45,10 @@ if __name__ == '__main__':
 			title = songsLines[i1 + 3]
 			year = songsLines[i1 + 4]
 			genres = songsLines[i1 + 5]
+			genres = genres.split("[")[1]
+			genres = genres.split("]")[0]
+			genres = [g.lstrip().rstrip()[2:-1] for g in genres.split(",")]			
+			
 			#Step 1: Extract .wav file
 			wavName = "%s.wav"%(filename.split(".m4a")[0])
 			filename = "%i/%s"%(dirNum, filename)
@@ -64,11 +72,23 @@ if __name__ == '__main__':
 			fields = lines.split(",")
 			fields = fields[0:-1]
 			temparffhandle.close()
+			#Step 4: Write the features to the ARFF file with all songs
 			for field in fields:
 				arffFile.write("%s,"%field)
 			arffFile.write("\"%s\",\"%s\",\"%s\",\"%s\"\n"%(genre.replace("\"", ""), artist.replace("\"", ""), album.replace("\"", ""), title.replace("\"", "")))
-			#Step 4: Remove the .wav file to free up space
+			#Step 5: Concatenate data to the numpy arrays
+			featuresArray = np.array([ [float(x) for x in fields] ])
+			if np.prod(songsFeatures.shape) == 0:
+				songsFeatures = featuresArray
+			else:
+				songsFeatures = np.concatenate([songsFeatures, featuresArray])
+			songsInfo.append({'filename':filename, 'artist':artist, 'album':album, 'title':title, 'year':int(year), 'genres':genres})
+			#Step 6: Remove the .wav file to free up space
 			if os.path.isfile(wavName):
 				os.remove(wavName)
 		dirNum = dirNum+1
+		#Save the matrix at intermediate steps
+		sio.savemat('', {'songsFeatures':songsFeatures, 'songsInfo':songsInfo, 'featureNames':attributes})
 		songsIndex.close()
+	os.chdir('../../MarsyasFeatures')
+	sio.savemat('', {'songsFeatures':songsFeatures, 'songsInfo':songsInfo, 'featureNames':attributes})
