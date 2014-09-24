898de23fb3e2af925cbad52b6ab1093020a031e6
ctralie <chris.tralie@gmail.com>
2014-09-24 11:50:01 -0400

    Saving birthing and killing edges along with other 1D rips filtration information now

--- DelaySeries/GTzan/computeGTzanRipsFiltrations.m
+++ DelaySeries/GTzan/computeGTzanRipsFiltrations.m
@@ -1,9 +1,9 @@
 %Programmer: Chris Tralie
-%Purpose: To extract the CAF and persistence features from the George
+%Purpose: To extract the CAF and 1D rips filtration features from the George
 %Tzanetakis 2002 dataset
 %indices: The indices of the genres to compute (Useful to run this file
 %on different cores with different indices to parallelize computation)
-function [] = computeGTzanFeatures(indices, SongsPerGenre, subsample, foldername)
+function [] = computeGTzanRipsFiltrations(indices, SongsPerGenre, subsample, foldername)
     addpath('genres');
     addpath('..');
     addpath('../chroma-ansyn');
@@ -29,13 +29,15 @@ function [] = computeGTzanFeatures(indices, SongsPerGenre, subsample, foldername
     
     AllPDs0 = cell(1, length(indices));
     AllPDs1 = cell(1, length(indices));
+    AllPDs1BK = cell(1, length(indices));
     featuresOrig = cell(1, length(indices));
     for ii = 1:length(indices)
        genre = genres{indices(ii)};
        fprintf(1, 'Doing %s...\n', genre);
        X = zeros(SongsPerGenre, 59*2);
-       PDs1 = cell(SongsPerGenre);
-       PDs0 = cell(SongsPerGenre);
+       PDs0 = cell(SongsPerGenre, 1);
+       PDs1 = cell(SongsPerGenre, 1);
+       PDs1BK = cell(SongsPerGenre, 1);
        parfor jj = 1:SongsPerGenre
            filename = sprintf('genres/%s/%s.%.5i.au', genre, genre, jj-1);
            DelaySeries = getDelaySeriesFeatures(filename, hopSize, 1, NWin);
@@ -50,14 +52,19 @@ function [] = computeGTzanFeatures(indices, SongsPerGenre, subsample, foldername
            DelaySeries = bsxfun(@times, DelaySeries, 1./ScaleSTDevs);
            %Do DGM1 separately for timbre, MFCC, and chroma
            %Subsample the point clouds by a factor of 2
-           [timbrePD1, timbrePD0] = getPersistenceDiagrams(DelaySeries(1:subsample:end, timbreIndices));
-           [MFCCPD1, MFCCPD0] = getPersistenceDiagrams(DelaySeries(1:subsample:end, MFCCIndices));
-           [ChromaPD1, ChromaPD0] = getPersistenceDiagrams(DelaySeries(1:subsample:end, ChromaIndices));
+           [timbrePD0, timbrePD1, timbrePD1BK] = getPersistenceDiagrams(DelaySeries(1:subsample:end, timbreIndices));
+           [MFCCPD0, MFCCPD1, MFCCPD1BK] = getPersistenceDiagrams(DelaySeries(1:subsample:end, MFCCIndices));
+           [ChromaPD0, ChromaPD1, ChromaPD1BK] = getPersistenceDiagrams(DelaySeries(1:subsample:end, ChromaIndices));
            fprintf(1, 'Finished %s %i\n', genre, jj);
-           PDs1{jj} = {timbrePD1, MFCCPD1, ChromaPD1};
            PDs0{jj} = {timbrePD0, MFCCPD0, ChromaPD0};
+           PDs1{jj} = {timbrePD1, MFCCPD1, ChromaPD1};
+           PDs1BK{jj} = {timbrePD1BK, MFCCPD1BK, ChromaPD1BK};
        end
-       save(sprintf('%s/GTzanFeatures%i.mat', foldername, indices(ii)), 'X', 'PDs1', 'PDs0', 'genres');
+       AllPDs0{ii} = PDs0;
+       AllPDs1{ii} = PDs1;
+       AllPDs1BK{ii} = PDs1BK;
+       featuresOrig{ii} = X;
+       save(sprintf('%s/GTzanFeatures%i.mat', foldername, indices(ii)), 'X', 'PDs0', 'PDs1', 'PDs1BK', 'genres');
     end
-    save(sprintf('%s/GTzanFeatures.mat', foldername), 'AllPDs0', 'AllPDs1', 'featuresOrig', 'genres');
+    save(sprintf('%s/GTzanFeatures.mat', foldername), 'AllPDs0', 'AllPDs1', 'AllPDs1BK', 'featuresOrig', 'genres');
 end
