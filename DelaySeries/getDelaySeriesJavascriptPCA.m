5b2d408d85db2f4f2701898a0b9447b04bd7670e
Chris Tralie <chris.tralie@gmail.com>
2014-10-19 00:15:09 -0400

    Adding matlab to javascript loop ditty conversion

---
+++ DelaySeries/getDelaySeriesJavascriptPCA.m
@@ -0,0 +1,26 @@
+function [] = getDelaySeriesJavascriptPCA( filename, hopSize, skipSize, windowSize, outprefix )
+    [DelaySeries, ~, SampleDelays] = getDelaySeriesFeatures( filename, hopSize, skipSize, windowSize );
+    
+    %Center on mean and scale by the standard deviation of each feature
+    DelaySeries = bsxfun(@minus, mean(DelaySeries), DelaySeries);
+    DelaySeries = bsxfun(@times, 1./std(DelaySeries), DelaySeries);
+    [~, DelaySeries] = pca(DelaySeries);
+
+    readSuccess = 0;
+    while readSuccess == 0
+        try
+            [X, Fs] = audioread(filename);
+            readSuccess = 1;
+        catch
+            readSuccess = 0;
+        end
+    end    
+    
+    SampleDelays = SampleDelays/Fs;
+    audiowrite(sprintf('%s.ogg', outprefix), X, Fs);
+    fout = fopen(sprintf('%s.txt', outprefix), 'w');
+    for ii = 1:size(DelaySeries, 1)
+       fprintf(fout, '%g,%g,%g,%g,', DelaySeries(ii, 1), DelaySeries(ii, 2), DelaySeries(ii, 3), SampleDelays(ii)); 
+    end
+    fclose(fout);
+end
