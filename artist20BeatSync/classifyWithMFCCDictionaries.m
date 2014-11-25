562cc0286d2a08004a5fc9b55d34bf481b163f0c
Chris Tralie <chris.tralie@gmail.com>
2014-11-24 20:59:44 -0500

    Trying 2 level classification

--- artist20BeatSync/classifyWithMFCCDictionaries.m
+++ artist20BeatSync/classifyWithMFCCDictionaries.m
@@ -29,12 +29,20 @@ end
 %Setup full dictionary
 load('MFCCDicts');
 D = cell2mat(Djs);
+
 param.K = NDictElems*NArtists;
 param.numThreads = 4;
 param.lambda = 0.15;
 param.iter = -1;
 
-C = zeros(NArtists, NArtists);%Confusion Matrix
+%Parameters for inter-class dictionarys
+paramPhi.K = NPoolDictElems;
+paramPhi.numThreads = 4;
+paramPhi.lambda = 1/NArtists;
+paramPhi.iter = 1000;
+
+CLevel1 = zeros(NArtists, NArtists);%Confusion Matrix level 1
+CLevel2 = zeros(NArtists, NArtists);%Confusion Matrix level 2
 for ii = 1:NArtists
     f = strsplit(songsByArtist{ii}{1}, '/');
     fprintf(1, 'Testing songs in %s\n', f{1});
@@ -44,16 +52,32 @@ for ii = 1:NArtists
         alpha = mexOMP(X', D, param);
         alpha = sum(abs(alpha), 2);
         alpha = reshape(alpha, NDictElems, NArtists);
-        scorePooled = sum(alpha, 1);
-%         plot(scorePooled);
-%         set(gca, 'XTick', 1:NArtists);
-%         set(gca, 'XTickLabel', artistNames);
+        scorePooled = full(sum(alpha, 1));
+        
+        %Level 1 classification
         [~, jj] = max(scorePooled);%argmax
         f = strsplit(songsByArtist{ii}{kk}, '/');
-        fprintf(1, '%s classified as %s\n', f{1}, artistNames{jj});
-        C(ii, jj) = C(ii, jj) + 1;
+        fprintf(1, 'Level 1: %s classified as %s\n', f{1}, artistNames{jj});
+        CLevel1(ii, jj) = CLevel1(ii, jj) + 1;
+        
+        %Level 2 classification
+        minObj = inf;
+        minClass = 1;
+        for jj = 1:length(Phijs)
+            alpha = mexOMP(scorePooled', Phijs{jj}, paramPhi);
+            fit = Phijs{jj}*alpha - scorePooled';
+            fit = sum(fit.*fit);
+            thisObj = 0.5*fit + paramPhi.lambda*sum(abs(alpha));
+            if thisObj < minObj
+                minObj = thisObj;
+                minClass = jj;
+            end
+        end
+        CLevel2(ii, minClass) = CLevel2(ii, minClass) + 1;
+        fprintf(1, 'Level 2: %s classified as %s\n', f{1}, artistNames{minClass});
     end
-    fprintf(1, '%g Percent Correct So Far\n', 100*sum(diag(C))/sum(C(:)));
+    fprintf(1, 'Level 1 %g Percent Correct So Far\n', 100*sum(diag(CLevel1))/sum(CLevel1(:)));
+    fprintf(1, 'Level 2 %g Percent Correct So Far\n', 100*sum(diag(CLevel2))/sum(CLevel2(:)));
 end
 
 artistNamesDisp = cell(size(artistNames));
@@ -63,7 +87,13 @@ for ii = 1:length(artistNames)
     artistNamesDisp{ii} = name(1:newLen);
 end
 
-imagesc(C);
+imagesc(CLevel1);
 set(gca, 'YLim', [0 NArtists+1], 'YTick', 1:NArtists, 'YTickLabel', artistNamesDisp);
 set(gca, 'XLim', [0 NArtists+1], 'XTick', 1:NArtists, 'XTickLabel', artistNamesDisp);
-title(sprintf('%g Percent Correct', 100*sum(diag(C))/sum(C(:))));
+title(sprintf('Level 1%g Percent Correct', 100*sum(diag(CLevel1))/sum(CLevel1(:))));
+
+figure;
+imagesc(CLevel2);
+set(gca, 'YLim', [0 NArtists+1], 'YTick', 1:NArtists, 'YTickLabel', artistNamesDisp);
+set(gca, 'XLim', [0 NArtists+1], 'XTick', 1:NArtists, 'XTickLabel', artistNamesDisp);
+title(sprintf('Level 2%g Percent Correct', 100*sum(diag(CLevel2))/sum(CLevel2(:))));
